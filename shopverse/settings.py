1014fa06f6f8cce7dfc32339b097e5884e9e135e
ravish <www.ravishraheja@gmail.com>
2025-12-01 01:08:26 +0530

    Fix Whitenoise static configuration

--- shopverse/settings.py
+++ shopverse/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
